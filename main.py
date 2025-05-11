68039accb216249b342c8f56a8dd84756e21ccbd
sairam-rachamadugu <ram.rachamadugu32@gmail.com>
2025-05-11 22:23:18 +0530

    added index route

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def get_tenders():
     with open("data.json", "r") as f:
         data = json.load(f)
     return jsonify(data)
+@app.route("/")
+def hello():
+    return "Flask API is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
